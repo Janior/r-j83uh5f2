50eec04aa2ae2a4920d318e5b070b60397ef153c
Janior <janior_orlando@hotmail.com>
2018-12-20 11:45:44 -0800

    Reto Contador de caracteres

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
+  constructor() {
+    super();
+    this.counter = this.counter.bind(this);
+    this.state = { value: 0 };
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" id="count" onChange={this.counter} />
+
+        <div className="counter" id="contador">
+          {this.state.value}
+        </div>
       </div>
     );
+  }
+
+  counter(e) {
+    this.setState({ value: e.target.value.length});
   }
 }
 
